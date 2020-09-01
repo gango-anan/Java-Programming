2c5561c7d6870bd80256a03479d0fddbf6a565d4
Galiwango <galiwango101@gmail.com>
2020-09-01 17:33:17 +0300

    Did some code clean up.

--- src/com/gangoinc/Main.java
+++ src/com/gangoinc/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENTAGE = 100;
-        int principal;
-        float annualInterestRate;
-        float monthlyInterestRate;
-        byte period;
-        int numberOfPayments;
+        int principal=0;
+        float annualInterestRate=0.0F;
+        float monthlyInterestRate=0.0F;
+        byte period=0;
+        int numberOfPayments=0;
 
                 Scanner getInput = new Scanner(System.in);
         while(true) {
@@ -25,23 +25,23 @@ public class Main {
         while(true) {
             System.out.print("Annual Interest Rate(0 - 30): ");
             annualInterestRate = getInput.nextFloat();
-            if(annualInterestRate > 0 && annualInterestRate <= 30)
+            if(annualInterestRate > 0 && annualInterestRate <= 30) {
+                monthlyInterestRate = annualInterestRate / PERCENTAGE / MONTHS_IN_YEAR;
                 break;
+            }
             System.out.println("Enter a value that is greater than 0 and less or equal to 30.");
         }
 
         while(true) {
             System.out.print("Period (1 - 30 Years): ");
             period = getInput.nextByte();
-            if(period >=1 && period <= 30)
+            if(period >=1 && period <= 30) {
+                numberOfPayments = period * MONTHS_IN_YEAR;
                 break;
+            }
             System.out.println("Enter a value between 1 and 30.");
         }
         System.out.println();
-
-        monthlyInterestRate = annualInterestRate / PERCENTAGE / MONTHS_IN_YEAR;
-        numberOfPayments = period * MONTHS_IN_YEAR;
-
         double mortgage = principal*(monthlyInterestRate*Math.pow(1+monthlyInterestRate,numberOfPayments))/
                                     (Math.pow(1+monthlyInterestRate,numberOfPayments) - 1);
 
