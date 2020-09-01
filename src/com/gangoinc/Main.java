93f5364c43b23bcee1a8f858307dbac1297a29ce
Galiwango <galiwango101@gmail.com>
2020-09-01 17:19:31 +0300

    Improved the flow of the error checking statements

--- src/com/gangoinc/Main.java
+++ src/com/gangoinc/Main.java
@@ -18,33 +18,29 @@ public class Main {
         while(true) {
             System.out.print("Principal ($1k - $1M): ");
             principal = getInput.nextInt();
-            if (principal < 1_000 || principal > 1_000_000)
-                System.out.println("Enter a value between $1,000 and $1,000,000.");
-            else
+            if (principal >= 1_000 && principal <= 1_000_000)
                 break;
+            System.out.println("Enter a value between $1,000 and $1,000,000.");
         }
         while(true) {
             System.out.print("Annual Interest Rate(0 - 30): ");
             annualInterestRate = getInput.nextFloat();
-            if(annualInterestRate<=0 || annualInterestRate>30)
-                System.out.println("Enter a value that is greater than 0 and less or equal to 30.");
-            else {
-                monthlyInterestRate = annualInterestRate / PERCENTAGE / MONTHS_IN_YEAR;
+            if(annualInterestRate > 0 && annualInterestRate <= 30)
                 break;
-            }
+            System.out.println("Enter a value that is greater than 0 and less or equal to 30.");
         }
 
         while(true) {
             System.out.print("Period (1 - 30 Years): ");
             period = getInput.nextByte();
-            if(period<1 || period > 30)
-                System.out.println("Enter a value between 1 and 30.");
-            else {
-                numberOfPayments = period * MONTHS_IN_YEAR;
+            if(period >=1 && period <= 30)
                 break;
-            }
+            System.out.println("Enter a value between 1 and 30.");
         }
         System.out.println();
+
+        monthlyInterestRate = annualInterestRate / PERCENTAGE / MONTHS_IN_YEAR;
+        numberOfPayments = period * MONTHS_IN_YEAR;
 
         double mortgage = principal*(monthlyInterestRate*Math.pow(1+monthlyInterestRate,numberOfPayments))/
                                     (Math.pow(1+monthlyInterestRate,numberOfPayments) - 1);
